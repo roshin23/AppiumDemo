d1c02b4b26d2c50c90b7850a928b5edb17a5f2c3
Roshin Surendran <roshin23@gmail.com>
2024-10-14 17:07:33 +0400

    Added cases related to Products page

--- src/test/java/MargoExpress/Mobile/Ecommerce_Tc_2.java
+++ src/test/java/MargoExpress/Mobile/Ecommerce_Tc_2.java
@@ -1,7 +1,13 @@
 package MargoExpress.Mobile;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -24,7 +30,30 @@ public class Ecommerce_Tc_2 extends BaseTest {
 		driver.hideKeyboard();
 		driver.findElement(By.id("com.androidsample.generalstore:id/radioMale")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-	}
 		
-
+		//add synchornization
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement pageTitleelem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.androidsample.generalstore:id/toolbar_title")));
+		
+//		WebElement pageTitleelem =  driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title"));
+		String actualPagetitle = pageTitleelem.getAttribute("text");
+		Assert.assertEquals(actualPagetitle, "Products");
+		
+	}
+	
+	@Test
+	public void selectProduct()
+	{
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"));"));
+		List<WebElement> productsEle =  driver.findElements(By.xpath("//android.widget.TextView"));
+		for (WebElement element : productsEle)
+		{
+			String ProductName = element.getAttribute("text");
+			if ProductName == ("Jordan 6 Rings")
+				{
+					driver.findElement(By.xpath("(//android.widget.TextView[@resource-id=\"com.androidsample.generalstore:id/productAddCart\"])")).get.click();	
+				}
+		}
+		
+	}
 }
